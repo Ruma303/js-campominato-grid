3d2c3ee7fbff56a345dddebd4f22ac73bc0d8376
Ruma <matteoh8@hotmail.it>
2022-11-02 17:23:24 +0100

    3 if da completare

--- js/script.js
+++ js/script.js
@@ -1,9 +1,42 @@
 const eleGrid = document.querySelector('.grid');
 const eleCell = document.querySelector('.cell');
+const select = document.querySelectorAll('#select');
+const diff1 = document.querySelector('#diff1');
+const diff2 = document.querySelector('#diff2');
+const diff3 = document.querySelector('#diff3');
+const btn = document.querySelector('#play-btn');
 
-//creazione celle nella griglia
-for (let i = 1; i < 101; i++) {
-    
+
+//aggiungi event listener sul bottone per generare la griglia in base alla difficoltà
+/*btn.addEventListener('click', function creatingCells() {
+    //Scelta difficoltà
+    if (select.value == 1) {
+        //creazione celle nella griglia
+        for (let i = 1; i < 101; i++) {
+            creatingCells();
+            eleCell.classList.add('hard');
+            eleCell.classList.remove('easy');
+            eleCell.classList.remove('medium');
+        } 
+    } else if (select.value == 2) {
+        for (let i = 1; i < 82; i++) {
+            creatingCells();
+            eleCell.classList.add('medium');
+            eleCell.classList.remove('easy');
+            eleCell.classList.remove('hard');
+        }
+    } else if (select.value == 3) {
+        for (let i = 1; i < 50; i++) {
+            creatingCells();
+            eleCell.classList.add('easy');
+            eleCell.classList.remove('medium');
+            eleCell.classList.remove('hard');
+      }}
+});*/
+
+//Funzione che crea le celle
+function creatingCells() {
+    for (let i = 1; i < 101; i++) {
     //creazione div e classe delle celle
     const eleCell = document.createElement('div');
     eleCell.classList.add('cell');
@@ -14,12 +47,12 @@ for (let i = 1; i < 101; i++) {
     eleCell.classList.add('number');
     eleCell.innerHTML += cellNumber;
 
-    
-    //funzione toogle
+    //funzione toogle per colorare i quadrati sul click
     eleCell.addEventListener('click', function(){
         this.classList.toggle('active');
-
         //output numero cliccato in console
         console.log(cellNumber)
-    });
-}
+    }); 
+}}
+
+creatingCells();
