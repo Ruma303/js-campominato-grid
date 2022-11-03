9e1da9ece5dd7fd668d8a45491f4b81e379ac097
Ruma <matteoh8@hotmail.it>
2022-11-03 10:51:52 +0100

    griglia dinamica, celle da aggiustare

--- js/script.js
+++ js/script.js
@@ -1,58 +1,42 @@
 const eleGrid = document.querySelector('.grid');
 const eleCell = document.querySelector('.cell');
-const select = document.querySelectorAll('#select');
-const diff1 = document.querySelector('#diff1');
-const diff2 = document.querySelector('#diff2');
-const diff3 = document.querySelector('#diff3');
+const title = document.querySelector('#title');
+const select = document.querySelector('#select');
 const btn = document.querySelector('#play-btn');
 
+//Bottone Play
+btn.addEventListener('click', function() {
+    //tolgo il titolo e mostro la griglia
+    eleGrid.classList.remove('hidden');
+    title.classList.add('hidden');
+    //Reset celle ad ogni click
+    eleGrid.innerHTML = '';
 
-//aggiungi event listener sul bottone per generare la griglia in base alla difficoltà
-/*btn.addEventListener('click', function creatingCells() {
-    //Scelta difficoltà
-    if (select.value == 1) {
-        //creazione celle nella griglia
-        for (let i = 1; i < 101; i++) {
-            creatingCells();
-            eleCell.classList.add('hard');
-            eleCell.classList.remove('easy');
-            eleCell.classList.remove('medium');
-        } 
-    } else if (select.value == 2) {
-        for (let i = 1; i < 82; i++) {
-            creatingCells();
-            eleCell.classList.add('medium');
-            eleCell.classList.remove('easy');
-            eleCell.classList.remove('hard');
-        }
-    } else if (select.value == 3) {
-        for (let i = 1; i < 50; i++) {
-            creatingCells();
-            eleCell.classList.add('easy');
-            eleCell.classList.remove('medium');
-            eleCell.classList.remove('hard');
-      }}
-});*/
+    //numero di celle scegliendo il value del selettore
+    const numCells = parseInt(select.value);
+    //calcolo radice quadrata
+    const sideSquare = Math.sqrt(numCells);
+    //identificare variabile CSS
+    eleGrid.style.setProperty('--sideSquare', sideSquare); 
+        for (let i = 1; i <= numCells; i++) { //trasforma 101 in limite variabile
+            //creazione div e classe delle celle
+            const eleCell = document.createElement('div');
+            eleCell.classList.add('cell');
+            
+            //creazione numeri
+            let cellNumber = i;
+            eleCell.classList.add('number');
+            eleCell.innerHTML += cellNumber;
+            
+            //calcolo celle
+            eleCell.style.width = `calc(100% / ${sideSquare});`
+            eleCell.style.height = `calc(100% / ${sideSquare});`
+            eleGrid.append(eleCell);
 
-//Funzione che crea le celle
-function creatingCells() {
-    for (let i = 1; i < 101; i++) {
-    //creazione div e classe delle celle
-    const eleCell = document.createElement('div');
-    eleCell.classList.add('cell');
-    eleGrid.append(eleCell);
-    
-    //creazione numeri
-    let cellNumber = i;
-    eleCell.classList.add('number');
-    eleCell.innerHTML += cellNumber;
-
-    //funzione toogle per colorare i quadrati sul click
-    eleCell.addEventListener('click', function(){
-        this.classList.toggle('active');
-        //output numero cliccato in console
-        console.log(cellNumber)
-    }); 
-}}
-
-creatingCells();
+            //funzione toogle per colorare i quadrati sul click
+            eleCell.addEventListener('click', function(){
+                this.classList.toggle('active');
+                //output numero cliccato in console
+                console.log(cellNumber)})
+            }
+})
